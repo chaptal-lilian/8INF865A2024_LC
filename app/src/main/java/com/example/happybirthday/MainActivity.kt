5ec73b72e38e8e68617bc7a574c7bb4afd2f35e1
Lilian CHAPTAL <chaptal.lilian@gmail.com>
2024-09-05 11:31:16 -0600

    2 activities finished

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -22,6 +22,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -39,8 +40,8 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     GreetingImage(
-                        message = "Happy Birthday Lilian!",
-                        from = "From Jacques",
+                        message = stringResource(R.string.happy_birthday_text),
+                        from = stringResource(R.string.signature_text),
                         modifier = Modifier.padding(8.dp))
                 }
             }
@@ -86,7 +87,7 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier){
             fontSize = 30.sp,
             modifier = Modifier
                 .padding(16.dp)
-                .align(alignment = Alignment.End)
+                .align(alignment = Alignment.CenterHorizontally)
         )
     }
 }
@@ -115,6 +116,6 @@ fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier){
 @Composable
 fun BirthdayCardPreview() {
     HappyBirthdayTheme {
-        GreetingImage(message = "Happy Birthday Lilian !", from = "From Jacques")
+        GreetingImage(message = stringResource(R.string.happy_birthday_text), from = stringResource(R.string.signature_text))
     }
 }
