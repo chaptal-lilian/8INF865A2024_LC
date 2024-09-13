16feb1ff5d2e8f74ca220dd0f5ad594c2c40692b
Lilian CHAPTAL <chaptal.lilian@gmail.com>
2024-09-13 15:42:54 -0400

    step 6 activity 2 + colors

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -8,20 +8,27 @@ import androidx.activity.enableEdgeToEdge
 import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
@@ -36,6 +43,7 @@ import java.text.NumberFormat
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 
@@ -59,9 +67,10 @@ class MainActivity : ComponentActivity() {
 fun TipTimeLayout() {
     var amountInput by remember { mutableStateOf("") }
     var tipInput by remember { mutableStateOf("") }
+    var roundUp by remember { mutableStateOf(false)}
     val amount = amountInput.toDoubleOrNull() ?: 0.0 // analyse en tant que double et renvoie, sinon null si c'est pas une représentation valide de nombre; operateur elvis ?: renvoie l'expression qui le precede si pas null et ce qui suit si null
     val tipPercent = tipInput.toDoubleOrNull() ?: 0.0
-    val tip = calculateTip(amount, tipPercent)
+    val tip = calculateTip(amount, tipPercent, roundUp)
     Column(
         modifier = Modifier
             .statusBarsPadding()
@@ -101,6 +110,11 @@ fun TipTimeLayout() {
                 .padding(bottom = 32.dp)
                 .fillMaxWidth()
         )
+        RoundTheTipRow( // parametre roundUp defini sur roundup et rappel lambda qui màj la valeur de roundUp
+            roundUp = roundUp,
+            onRoundUpChanged = { roundUp = it },
+            modifier = Modifier.padding(bottom = 32.dp)
+        )
         Text(
             text = stringResource(R.string.tip_amount, tip),// propriété tip pour afficher le montant du pourboire
             style = MaterialTheme.typography.displaySmall
@@ -109,6 +123,7 @@ fun TipTimeLayout() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun EditNumberField(
     @StringRes label: Int,//ref de ressource string.xml;
@@ -127,17 +142,56 @@ fun EditNumberField(
         label = { Text(stringResource(label))}, // accepte la valeur de la ressource bill_amount et affiche comme un placeholder
         singleLine = true,// une seule ligne à faire défiler
         keyboardOptions = keyboardOptions,
-        modifier = modifier
+        modifier = modifier,
+        colors = TextFieldDefaults.textFieldColors(
+            containerColor = Color.LightGray
+        )
     )
 }
+
+@Composable
+fun RoundTheTipRow(
+    roundUp: Boolean,
+    onRoundUpChanged: (Boolean) -> Unit,// hisse l'etat du btn bascule
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .size(48.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = stringResource(R.string.round_up_tip)
+        )
+        Switch(
+            colors = SwitchDefaults.colors(
+                checkedThumbColor = Color.Green,
+                uncheckedThumbColor = Color.Red,
+                checkedTrackColor = Color.LightGray,
+                uncheckedTrackColor = Color.Gray
+            ),
+            modifier = modifier
+                .fillMaxWidth()
+                .wrapContentWidth(Alignment.End),
+            checked = roundUp, // check si bouton activé
+            onCheckedChange = onRoundUpChanged, // rappel à appeler en cas de clic sur le btn
+        )
+    }
+}
+
 
 /**
  * Calculates the tip based on the user input and format the tip amount
  * according to the local currency.
  * Example would be "$10.00".
  */
-private fun calculateTip(amount: Double, tipPercent: Double = 15.0): String {
-    val tip = tipPercent / 100 * amount
+private fun calculateTip(amount: Double, tipPercent: Double = 15.0, roundUp: Boolean): String {
+    //val tip = tipPercent / 100 * amount // erreur avec cette ligne apres roundup
+    var tip = tipPercent / 100 * amount
+    if(roundUp){
+        tip = kotlin.math.ceil(tip) // arrondit si btn basule d'arrondi activé
+    }
     return NumberFormat.getCurrencyInstance().format(tip)
 }
 
